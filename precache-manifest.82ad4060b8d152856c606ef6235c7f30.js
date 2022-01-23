82a5189312631748be7ab218a34979163681028c
Saim-282 <84862077+Saim-282@users.noreply.github.com>
2022-01-23 20:03:29 +0500

    hosted

---
+++ precache-manifest.82ad4060b8d152856c606ef6235c7f30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243d1b810e084afbbdba5ae49463ce6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4c8c2a85eeb9b8efcb2",
+    "url": "/static/css/main.8335f4c2.chunk.css"
+  },
+  {
+    "revision": "d0632374f1df33b2a1bd",
+    "url": "/static/js/2.e42cc546.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e42cc546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4c8c2a85eeb9b8efcb2",
+    "url": "/static/js/main.b09e6363.chunk.js"
+  },
+  {
+    "revision": "329102c932a6288a601d",
+    "url": "/static/js/runtime-main.c0c6e36b.js"
+  }
+]);
